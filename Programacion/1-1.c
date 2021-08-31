5f63b569404c9da02d630d297842f8fdd097e1f5
Edgar Luna <edgarluna16@hotmail.com>
2021-08-31 08:41:40 -0300

    primer commit

---
+++ Programacion/1-1.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int a, b, c;
+
+    printf("Ingrese primer numero\n");
+    scanf("%d", &a);
+    printf("Ingrese segundo numero\n");
+    scanf("%d", &b);
+
+    c=a+b;
+    printf("La suma de %d y %d es %d", a, b, c);
+
+    return 0;
+}
